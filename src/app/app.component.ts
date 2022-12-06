e9a98b0f3b64373c73ae01ed1c7e91a1368b60ab
loppies <96416715+loppies@users.noreply.github.com>
2022-12-06 20:44:11 +0100

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ import { RandomService } from './random/random.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  constructor(private myService: RandomService) {
+  }
+
   text = 'people';
   ran1 = 'Random Component max = 100';
   cos = new RandomService();
@@ -16,15 +19,32 @@ export class AppComponent implements OnInit {
 
   getRandom() {
     this.ran11 = this.cos.getRandomInt(100);
-    let el = document.getElementById("half");
-    console.log(el.innerHTML);
+    let el = document.getElementById('half');
+    console.log(el);
+    if (this.ran11 > 50) {
+      el.innerHTML = 'Górna połowa wartości';
+      el.style.color = 'red';
+    } else {
+      el.innerHTML = 'Dolna połowa wartości';
+      el.style.color = 'green';
+    }
+    return this.ran11;
   }
 
   getRandom1() {
     this.ran22 = this.cos.getRandomInt(10);
+    let el = document.getElementById('half1');
+    if (this.ran22 > 5) {
+      el.innerHTML = 'Górna połowa wartości';
+      el.style.color = 'red';
+    } else {
+      el.innerHTML = 'Dolna połowa wartości';
+      el.style.color = 'green';
+    }
   }
 
-  ngOnInit(): void {}
-
-  constructor(private myService: RandomService) {}
+  ngOnInit(): void { 
+    this.getRandom();
+    this.getRandom1();
+  }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+  ids = 0;
+  constructor() {}
 
-  constructor() { }
+  people = [];
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  deletePerson() {
+    console.log('elo');
   }
 
+  addPerson(newPerson: string) {
+    if (newPerson) {
+      this.people.push(newPerson);
+    }
+    let el = document.getElementById('lista');
+    let li = document.createElement('li');
+    li.setAttribute('id', 'li' + this.ids.toString());
+    li.innerHTML = this.people[this.ids] + '  ';
+    let a = document.createElement('a');
+    a.innerHTML = 'delete';
+    a.setAttribute('href', '#');
+    a.setAttribute('id', this.ids.toString());
+    a.setAttribute('onclick', 'this.deletePerson()');
+    li.appendChild(a);
+    el.appendChild(li);
+    this.ids++;
+    console.log(li.id);
+  }
 }
