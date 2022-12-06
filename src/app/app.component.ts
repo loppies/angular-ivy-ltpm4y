5f46798a5e54e259c40512341466d5b049ea59ef
loppies <96416715+loppies@users.noreply.github.com>
2022-12-06 11:43:14 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent implements OnInit{
   name = 'Angular ' + VERSION.major;
+  text = "people";
+
+  ngOnInit(): void { 
+  }
 }
